67519072d42c93b9eb54bcda8dc7637b0d4cc7b0
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-05-16 00:49:52 +0200

    Create script mount secrets

--- mount.sh
+++ mount.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
 
+LV_NAME="${LV_NAME-"/dev/mapper/mysecret-lvol0"}"
+LV_NAME_OPEN="${LV_NAME_OPEN-"securelv"}"
+
+# arg: -f
+PARTITIONS=()
+
+
+# Read all arguments
+while [[ -n "$1" ]]; do
+	case "$1" in
+		-d)
+			DESTINATION_MOUNT="$2";
+			shift;
+			;;
+		--user)
+			USER_GROUP="$2"
+			shift;
+			;;
+		--help)
+			echo "--help        Print this message";
+			echo "-d            Destination mount";
+			echo "--user        User";
+			exit 1;
+			;;
+		*)
+			break;
+			;;
+	esac
+	shift;
+done
+
+DESTINATION_MOUNT="${DESTINATION_MOUNT?"Mandatory parameter destination"}"
+USER_GROUP="${USER_GROUP?"Mandatory parameter user"}"
+
+PARTITIONS+=( "$@" )
+
 # Setup loop in images
-losetup -P -f "$PWD/1-10G.partition"
+for image in "${PARTITIONS[@]}"; do
+	losetup -P -f "$image"
+done
 
 # Scan and enable Group volumes
 vgscan
 vgchange -ay
 
 # Mount encrypted volume
-cryptsetup open /dev/mapper/mysecret-lvol0 securelv
-mount /dev/mapper/securelv /home/lorenzo/secrets/data/
-chown lorenzo:lorenzo /home/lorenzo/secrets/data/
+cryptsetup open "$LV_NAME" "$LV_NAME_OPEN"
+mount /dev/mapper/${LV_NAME_OPEN} "$DESTINATION_MOUNT"
+chown "$USER_GROUP" "$DESTINATION_MOUNT"
